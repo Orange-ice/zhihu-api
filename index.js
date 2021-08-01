628d67b777d65ca523d5d688164a224acb3d1b9c
Burt <2902978956@qq.com>
2021-08-01 16:11:48 +0800

    使用koa-json-error进行错误信息处理配置

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const Koa = require('koa')
 const bodyparser = require('koa-bodyparser')
+const error = require('koa-json-error')
 const app = new Koa()
 
 const registerRoute = require('./app/routes/index')
 
+// 根据不同环境输出不同的错误处理信息
+app.use(error({
+    postFormat: (e, {stack, ...rest}) => process.env.NODE_ENV === 'production' ? rest : {stack, rest}
+  }
+))
 app.use(bodyparser())
 registerRoute(app)
 
