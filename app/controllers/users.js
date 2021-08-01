5400b8e81e831f441affc661cfc1be19aa2eb162
Burt <2902978956@qq.com>
2021-08-01 20:45:14 +0800

    user的增删改查

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -1,12 +1,47 @@
+const User = require('../models/users')
+
 class UsersController {
-  gainList(ctx) {
-    ctx.body = '这是用户列表'
+  // 查询用户列表
+  async find(ctx) {
+    ctx.body = await User.find()
   }
-  create(ctx) {
+
+  // 查询特定用户
+  async findById(ctx) {
+    const user = await User.findById(ctx.params.id)
+    if (!user) {
+      ctx.throw(404, '用户不存在')
+    }
+    ctx.body = user
+  }
+
+  // 新建用户
+  async create(ctx) {
     ctx.verifyParams({
       name: {type: 'string', required: true}
     })
-    ctx.body = {message: 'success'}
+    ctx.body = await new User(ctx.request.body).save()
+  }
+
+  // 更新用户
+  async update(ctx) {
+    ctx.verifyParams({
+      name: {type: 'string', required: true}
+    })
+    const user = await User.findByIdAndUpdate(ctx.params.id, ctx.request.body)
+    if (!user) {
+      ctx.throw(404)
+    }
+    ctx.body = user
+  }
+
+  // 删除用户
+  async remove(ctx) {
+    const user = await User.findByIdAndRemove(ctx.params.id)
+    if (!user) {
+      ctx.throw(404)
+    }
+    ctx.status = 204
   }
 }
 
--- app/routes/users.js
+++ app/routes/users.js
@@ -1,8 +1,11 @@
 const Router = require('koa-router')
 const router = new Router({prefix: '/users'}) // 设置路由前缀
-const {gainList, create} = require('../controllers/users')
+const {find, findById, create, update, remove} = require('../controllers/users')
 
-router.get('/', gainList)
+router.get('/', find)
+router.get('/:id', findById)
 router.post('/', create)
+router.put('/:id', update)
+router.delete('/:id', remove)
 
 module.exports = router
