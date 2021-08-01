7b1777ea3edec839fc7067aa4d386c38f966f6d1
Burt <2902978956@qq.com>
2021-08-01 19:29:35 +0800

    使用mongoose连接mongoDB

---
+++ app/config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  connectionStr: 'mongodb+srv://burt:jing5213@zhihu.m23s0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+}
--- index.js
+++ index.js
@@ -3,9 +3,18 @@ const chalk = require('chalk')
 const bodyparser = require('koa-bodyparser')
 const error = require('koa-json-error')
 const parameter = require('koa-parameter')
-const app = new Koa()
+const mongoose = require('mongoose')
 
 const registerRoute = require('./app/routes/index')
+const {connectionStr} = require('./app/config')
+const app = new Koa()
+
+// 连接mongoDB
+mongoose.connect(connectionStr, {useUnifiedTopology: true,  useNewUrlParser: true}, () => {
+  console.log(chalk.magenta('mongoDB is connected successfully!'))
+})
+// 连接mongoDB的错误处理
+mongoose.connection.on('error', console.error)
 
 // 根据不同环境输出不同的错误处理信息
 app.use(error({
@@ -17,5 +26,5 @@ app.use(parameter(app))
 registerRoute(app)
 
 app.listen(3000, () => {
-  console.log(chalk.cyan('app is listening 3000...'))
+  console.log(chalk.magenta('app is listening 3000...'))
 })
