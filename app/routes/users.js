d65819adb903eacf4b11616c7c0e0a7e67930dbd
Burt <2902978956@qq.com>
2021-08-03 20:53:05 +0800

    使用koa-jwt替换手动实现的认证授权中间件

--- app/routes/users.js
+++ app/routes/users.js
@@ -1,20 +1,10 @@
 const Router = require('koa-router')
-const jsonwebtoken = require('jsonwebtoken')
+const jwt = require('koa-jwt')
 const router = new Router({prefix: '/users'}) // 设置路由前缀
 const {find, findById, create, update, remove, login, checkOwner} = require('../controllers/users')
 const {secret} = require('../config')
 
-const auth = async (ctx, next) => {
-  const {authorization = ''} = ctx.request.header
-  const token = authorization.replace('Bearer ', '')
-  try {
-    const user = jsonwebtoken.verify(token, secret)
-    ctx.state.user = user
-  } catch (error) {
-    ctx.throw(401, error.message)
-  }
-  await next()
-}
+const auth = jwt({secret})
 
 router.get('/', find)
 router.get('/:id', findById)
