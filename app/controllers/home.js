381495299d85047a6a8d8d1c8c7b0673e974c444
Burt <2902978956@qq.com>
2021-08-05 20:37:28 +0800

    实现文件上传

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,6 +1,14 @@
+const path = require('path')
+
 class HomeController {
-  index(ctx){
+  index(ctx) {
     ctx.body = '这是用户列表'
+  }
+
+  upload(ctx) {
+    const file = ctx.request.files.file
+    const basename = path.basename(file.path)
+    ctx.body = {url: `${ctx.origin}/uploads/${basename}`}
   }
 }
 
--- index.js
+++ index.js
@@ -1,27 +1,42 @@
 const Koa = require('koa')
 const chalk = require('chalk')
-const bodyparser = require('koa-bodyparser')
+const koaBody = require('koa-body')
+const koaStatic = require('koa-static')
 const error = require('koa-json-error')
 const parameter = require('koa-parameter')
 const mongoose = require('mongoose')
+const path = require('path')
 
 const registerRoute = require('./app/routes/index')
 const {connectionStr} = require('./app/config')
 const app = new Koa()
 
 // 连接mongoDB
-mongoose.connect(connectionStr, {useUnifiedTopology: true,  useNewUrlParser: true}, () => {
+mongoose.connect(connectionStr, {useUnifiedTopology: true, useNewUrlParser: true}, () => {
   console.log(chalk.magenta('mongoDB is connected successfully!'))
 })
 // 连接mongoDB的错误处理
 mongoose.connection.on('error', console.error)
+
+app.use(koaStatic(path.join(__dirname, 'app/public')))
 
 // 根据不同环境输出不同的错误处理信息
 app.use(error({
     postFormat: (e, {stack, ...rest}) => process.env.NODE_ENV === 'production' ? rest : {stack, rest}
   }
 ))
-app.use(bodyparser())
+
+app.use(koaBody({
+  // 支持文件格式
+  multipart: true,
+  formidable: {
+    // 上传目录
+    uploadDir: path.join(__dirname, '/app/public/uploads'),
+    // 保留文件扩展名
+    keepExtensions: true,
+  }
+}))
+
 app.use(parameter(app))
 registerRoute(app)
 
