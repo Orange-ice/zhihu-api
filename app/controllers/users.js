bb4066195c1791b96e86dbec618a8133502e500b
Burt <2902978956@qq.com>
2021-08-07 13:04:18 +0800

    个人资料需求实现

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -2,6 +2,8 @@ const jwt = require('jsonwebtoken')
 const User = require('../models/users')
 const {secret} = require('../config')
 
+const selects = '+locations +business +employment +educations'
+
 class UsersController {
   // 查询用户列表
   async find(ctx) {
@@ -10,7 +12,7 @@ class UsersController {
 
   // 查询特定用户
   async findById(ctx) {
-    const user = await User.findById(ctx.params.id)
+    const user = await User.findById(ctx.params.id).select(selects)
     if (!user) {
       ctx.throw(404, '用户不存在')
     }
@@ -37,9 +39,19 @@ class UsersController {
   async update(ctx) {
     ctx.verifyParams({
       name: {type: 'string', required: false},
-      password: {type: 'string', required: false}
+      password: {type: 'string', required: false},
+      avatar: {type: 'string', required: false},
+      gender: {type: 'string', required: false},
+      headline: {type: 'string', required: false},
+      locations: {type: 'array', itemType: 'string', required: false},
+      business: {type: 'string', required: false},
+      employment: {type: 'array', itemType: 'object', required: false},
+      educations: {type: 'array', itemType: 'object', required: false}
     })
-    const user = await User.findByIdAndUpdate(ctx.params.id, ctx.request.body)
+    const user = await User.findByIdAndUpdate(ctx.params.id, ctx.request.body, {
+      new: true, // 返回修改后的对象
+      useFindAndModify: false
+    }).select(selects)
     if (!user) {
       ctx.throw(404)
     }
@@ -69,6 +81,7 @@ class UsersController {
     ctx.body = {token}
   }
 
+  // 检查当前登录用户是否操作的是自己
   async checkOwner(ctx, next) {
     if (ctx.params.id !== ctx.state.user._id) {ctx.throw(403, '没有权限')}
     await next()
--- app/models/users.js
+++ app/models/users.js
@@ -1,10 +1,43 @@
 const mongoose = require('mongoose')
 const {Schema, model} = mongoose
-
+/**
+ * @param name 姓名
+ * @param password 密码
+ * @param avatar 头像的url
+ * @param gender 性别
+ * @param headline 个人的一句话介绍
+ * @param business 行业
+ * @param employment 职业经历 (公司，岗位，开始时间，结束时间)
+ * @param educations 教育经历 (学校，专业，学历【1 高中及以下，2 大专，3 本科，4 硕士，5 博士及以上】,开始时间，结束时间)
+ * */
 const UserSchema = new Schema({
   __v: {type: Number, select: false},
   name: {type: String, required: true},
-  password: {type: String, required: true, select: false}
+  password: {type: String, required: true, select: false},
+  avatar: {type: String},
+  gender: {type: String, enum: ['male', 'female']},
+  headline: {type: String},
+  locations: {type: [{type: String}], select: false},
+  business: {type: String, select: false},
+  employment: {
+    type: [{
+      company: {type: String},
+      job: {type: String},
+      startDate: {type: String},
+      endDate: {type: String}
+    }],
+    select: false
+  },
+  educations: {
+    type: [{
+      school: {type: String},
+      major: {type: String},
+      diploma: {type: Number, enum: [1, 2, 3, 4, 5]},
+      startDate: {type: String},
+      endDate: {type: String}
+    }],
+    select: false
+  }
 })
 
 module.exports = model('User', UserSchema)
